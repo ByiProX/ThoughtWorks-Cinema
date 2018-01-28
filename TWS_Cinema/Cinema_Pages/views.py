5d0f66067c26354ee93d9c4e0944552cb150ce02
Quentin Wang <wangkx0105@outlook.com>
2018-01-29 00:43:07 +0800

    annotation api

--- TWS_Cinema/Cinema_Pages/views.py
+++ TWS_Cinema/Cinema_Pages/views.py
@@ -3,7 +3,6 @@ from .models import Movie
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from django.http import Http404
-from urllib.parse import unquote
 
 # Create your views here.
 
@@ -12,61 +11,53 @@ def index(request):
     return render(request, 'index.html', locals())
 
 # API
-
-#
-def searchbyid(request, id):
-    try:
-        data = model_to_dict(Movie.objects.get(id=id))
-    except:
-        raise Http404("Movie does not exist!!!!!!!!!!!!!")
-    return JsonResponse(data, safe=False)
-
-# a new api for reference
-# def searchbyid(request):
+# def searchbyid(request, id):
 #     try:
-#         id = request.GET.get('id')
 #         data = model_to_dict(Movie.objects.get(id=id))
 #     except:
 #         raise Http404("Movie does not exist!!!!!!!!!!!!!")
 #     return JsonResponse(data, safe=False)
-
-
-def searchbytitle(request, title):
-    try:
-        data = model_to_dict(Movie.objects.get(title=title))
-        return JsonResponse(data, safe=False)
-    except:
-        raise Http404("Movie does not exist!!!!!!!!!!!!!")
-
-
-def searchbyoriginal_title(request, original_title):
-    try:
-        data = model_to_dict(Movie.objects.get(original_title=original_title))
-        return JsonResponse(data, safe=False)
-    except:
-        raise Http404("Movie does not exist!!!!!!!!!!!!!")
-
-
-def searchbygenre(request, genre):
-    try:
-        data = list(Movie.objects.all())
-        find = []
-        json = {}
-        for d in data:
-            if genre in model_to_dict(d)['genres']:
-                find.append(model_to_dict(d))
-        json['subject'] = find
-        return JsonResponse(json, safe=False)
-    except:
-        raise Http404("Movie does not exist!!!!!!!!!!!!!")
-
-
-
-
-
-
-
-
+#
+#
+# # a new api for reference
+# # def searchbyid(request):
+# #     try:
+# #         id = request.GET.get('id')
+# #         data = model_to_dict(Movie.objects.get(id=id))
+# #     except:
+# #         raise Http404("Movie does not exist!!!!!!!!!!!!!")
+# #     return JsonResponse(data, safe=False)
+#
+#
+# def searchbytitle(request, title):
+#     try:
+#         data = model_to_dict(Movie.objects.get(title=title))
+#         return JsonResponse(data, safe=False)
+#     except:
+#         raise Http404("Movie does not exist!!!!!!!!!!!!!")
+#
+#
+# def searchbyoriginal_title(request, original_title):
+#     try:
+#         data = model_to_dict(Movie.objects.get(original_title=original_title))
+#         return JsonResponse(data, safe=False)
+#     except:
+#         raise Http404("Movie does not exist!!!!!!!!!!!!!")
+#
+#
+# def searchbygenre(request, genre):
+#     try:
+#         data = list(Movie.objects.all())
+#         find = []
+#         json = {}
+#         for d in data:
+#             if genre in model_to_dict(d)['genres']:
+#                 find.append(model_to_dict(d))
+#         json['subject'] = find
+#         return JsonResponse(json, safe=False)
+#     except:
+#         raise Http404("Movie does not exist!!!!!!!!!!!!!")
+#
 
 
 
